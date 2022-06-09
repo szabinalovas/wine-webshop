2f0231df538abca7bc7b0bad641833046ba42802
szabinalovas <szabinaa.lovas@gmail.com>
2022-06-09 18:08:23 +0200

    init

---
+++ src/main/java/com/codecool/winewebshop/WineWebshopApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.winewebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineWebshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WineWebshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codecool/winewebshop/WineWebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.winewebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WineWebshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
